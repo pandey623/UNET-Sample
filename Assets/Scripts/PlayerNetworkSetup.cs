b74f11a6f6fc409b4cc539e7dc390559d3b35ed5
Vasco Gomes <ovascogomes@gmail.com>
2016-04-11 20:38:46 +0100

    Project setup, added base network components

---
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+using System.Collections;
+
+public class PlayerNetworkSetup : NetworkBehaviour
+{
+    [SerializeField] private Camera fpsCamera;
+    [SerializeField] private AudioListener audioListener;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            // disable scene camera
+            GameObject camera = GameObject.Find("Scene Camera");
+
+            if (camera)
+            {
+                camera.GetComponent<AudioListener>().enabled = false;
+                camera.GetComponent<Camera>().enabled = false;
+            }
+
+            // enable fps camera
+            GetComponent<FirstPersonController>().enabled = true;
+            GetComponent<CharacterController>().enabled = true;
+
+            fpsCamera.enabled = true;
+            audioListener.enabled = true;
+
+            // set player name
+            gameObject.name = "Player " + netId;
+        }
+    }
+}
